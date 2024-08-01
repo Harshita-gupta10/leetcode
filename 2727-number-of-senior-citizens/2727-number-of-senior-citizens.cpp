d08cf1757af025defb46535d856fdf0848382c35
Harshita Gupta <91972448+Harshita-gupta10@users.noreply.github.com>
2024-08-01 14:37:18 +0530

    Time: 4 ms (82.75%), Space: 17.6 MB (16.71%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int ans=0;
-        for(int i=0; i < details.size(); i++)
+        int count = 0;
+        for(auto detail : details)
         {
-            int age=((details[i][11]-'0')*10) + (details[i][12]-'0');
-            ans+=(age>60);
+            if((detail[11] > '6')||(detail[11] == '6' && detail[12] != '0')) count++;
         }
-        return ans;
+        return count;
     }
 };
